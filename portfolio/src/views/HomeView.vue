4d0fb428506bc45ee59d31fc2b4f7659092ad95c
PaquierThomas <95091416+PaquierThomas@users.noreply.github.com>
2023-03-15 22:15:11 +0100

    première partie header

--- portfolio/src/views/HomeView.vue
+++ portfolio/src/views/HomeView.vue
@@ -2,13 +2,18 @@
 </script>
 
 <template>
-  <main>
-    <h1>Beetles</h1>
-    <h2>External morphology</h2>
-        <h3>Head</h3>
-            <h4>Mouthparts</h4>
-        <h3>Thorax</h3>
-            <h4>Prothorax</h4>
-            <h4>Pterothorax</h4>
-  </main>
+  <body>
+
+    <header>
+      <a href="#" class="brand">Thomas Paquier</a>
+      <div class="navigation">
+        <div class="navigation-items">
+          <a href="#">Accueil</a>
+          <a href="#">Projets</a>
+          <a href="#">A propos</a>
+          <a href="#">Contact</a>
+        </div>
+      </div>
+    </header>
+  </body>
 </template>
